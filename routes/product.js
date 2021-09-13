b13c9b166233ce747b4b691e7e0ac6301ff1fdf3
thanh tung phi <thanhtung19041998@gmail.com>
2021-09-13 16:14:00 +0700

    update get product

--- routes/product.js
+++ routes/product.js
@@ -11,40 +11,58 @@ router.get("/product/:id", async (req, res) => {
     }
 });
 
-router.get("/allproducts", async (req, res) => {
-    try {
-        const product = await Product.find()
-        res.status(200).json(product)
-    } catch (error) {
-        res.status(500).json(error)
-    }
-})
+// router.get("/", async (req, res) => {
+//     try {
+//         const product = await Product.find()
+//         const result = {
+//             product: product
+//         }
+//         res.status(200).json(result)
+//     } catch (error) {
+//         res.status(500).json(error)
+//     }
+// })
 
 router.get("/", async (req, res) => {
+
+
 
     try {
 
         const queryParam = queryString.parse(req._parsedUrl.query)
 
 
+
+        if (queryParam._page || queryParam._limit) {
+            // console.log(queryParam._page)
+            const _skip = (queryParam._page - 1) * queryParam._limit || 10
+            const _limit = Number.parseInt(queryParam._limit) || 10
+            const productLength = await Product.find();
+
+            const product = await Product.find().limit(_limit).skip(_skip).exec()
+
+
+            const result = {
+                product: product,
+                pagination: {
+                    _page: Number.parseInt(queryParam._page) || 1,
+                    _limit: _limit,
+                    _totalRow: Number.parseInt(productLength.length),
+                }
+            }
+            res.status(200).jsonp(result);
+        }
+        else {
+            // console.log("No Query Params")
+            const product = await Product.find()
+            const result = {
+                product: product
+            }
+            res.status(200).json(result)
+        }
         // console.log(queryParam);
 
-        const _skip = (queryParam._page - 1) * queryParam._limit || 10
-        const _limit = Number.parseInt(queryParam._limit) || 10
-        const productLength = await Product.find();
 
-        const product = await Product.find().limit(_limit).skip(_skip).exec()
-
-
-        const result = {
-            product: product,
-            pagination: {
-                _page: Number.parseInt(queryParam._page) || 1,
-                _limit: _limit,
-                _totalRow: Number.parseInt(productLength.length),
-            }
-        }
-        res.status(200).jsonp(result);
 
 
     } catch (error) {
