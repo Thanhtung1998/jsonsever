fdb5c36f97c0959ce4aa52771fbc81afc11df467
thanh tung phi <thanhtung19041998@gmail.com>
2021-09-13 15:02:02 +0700

    update pagination

--- routes/product.js
+++ routes/product.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const Product = require('../models/Product')
+const queryString = require('query-string')
 
 router.get("/:id", async (req, res) => {
     try {
@@ -10,13 +11,43 @@ router.get("/:id", async (req, res) => {
     }
 });
 
+// router.get("/", async (req, res) => {
+//     try {
+//         const product = await Product.find()
+//         res.status(200).json(product)
+//     } catch (error) {
+//         res.status(500).json(error)
+//     }
+// })
+
 router.get("/", async (req, res) => {
+
     try {
-        const product = await Product.find()
-        res.status(200).json(product)
+
+        const queryParam = queryString.parse(req._parsedUrl.query)
+        // console.log(queryParam);
+
+        const _skip = (queryParam._page - 1) * queryParam._limit
+
+        const productLength = await Product.find();
+
+        const product = await Product.find().limit(Number(queryParam._limit)).skip(_skip).exec()
+
+
+        const result = {
+            product: product,
+            pagination: {
+                _page: queryParam._page || 1,
+                _limit: queryParam._limit || 10,
+                _totalRow: Number.parseInt(productLength.length),
+            }
+        }
+        res.status(200).jsonp(result);
+
     } catch (error) {
-        res.status(500).json(error)
+        res.status(500).jsonp(error)
     }
+
 })
 
 module.exports = router;
